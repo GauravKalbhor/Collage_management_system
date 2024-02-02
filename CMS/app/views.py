eadacded44adfd840014a59a723a0ef5c971c89c
Gaurav Kalbhor <gauravkalbhor81@gmail.com>
2024-02-02 15:26:05 +0530

    css add

--- CMS/app/views.py
+++ CMS/app/views.py
@@ -12,12 +12,13 @@ def adminLogin(request):
     if request.method == "POST":
         adminID = request.POST['adminID']
         adminPassword = request.POST['adminPassword']
-
+        
         id = 'admin@gmail.com'
         password = 'admin$1234'
         # data = AdminLogin.objects.filter(id=adminID)
         if (id==adminID and password==adminPassword):
             # login_data = AdminLogin.objects.get(id=adminID)
+            
             if password == adminPassword:
                 return render(request,'app/home.html')
             else:
