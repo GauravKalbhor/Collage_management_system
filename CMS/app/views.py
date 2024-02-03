046d9911bfe96f210c228c0c944ff6534a9ad40b
Gaurav Kalbhor <gauravkalbhor81@gmail.com>
2024-02-03 15:09:47 +0530

    model and form created

--- CMS/app/views.py
+++ CMS/app/views.py
@@ -10,23 +10,30 @@ def home(request):
 
 def adminLogin(request):
     if request.method == "POST":
-        adminID = request.POST['adminID']
-        adminPassword = request.POST['adminPassword']
-        
-        id = 'admin@gmail.com'
-        password = 'admin$1234'
-        # data = AdminLogin.objects.filter(id=adminID)
-        if (id==adminID and password==adminPassword):
-            # login_data = AdminLogin.objects.get(id=adminID)
-            
-            if password == adminPassword:
-                return render(request,'app/home.html')
-            else:
-                message = "Email id or Password Not match"
-                return render(request,'admin/adminLogin.html',{'msg':message})
+        ad_ID = request.POST['adminID']
+        ad_pass = request.POST['adminPassword']
+
+        admin_data = {
+            'id' : 'admin@cybrom.co.in',
+            'password' : 'admin@1234'
+        }
+        if (admin_data['id'] == ad_ID and admin_data['password'] == ad_pass):
+            return render(request,'app/home.html')
         else:
-            form = Admin_form()
-            return render(request,'admin/adminLogin.html',{'form':form,'msg':message})
+            admin_form = Admin_form()
+            return render(request,'admin/adminLogin.html',{'form':admin_form})
     else:
-        form = Admin_form()
-        return render(request,'admin/adminLogin.html',{'form':form})
+        admin_form = Admin_form()
+        return render(request,'admin/adminLogin.html',{'form':admin_form})
+    
+def student(request):
+    if request.method == "POST":
+        stu_data = Student_Form(request.POST)
+        if stu_data.is_valid():
+            stu_data.save()
+            message = "Student details register successfully"
+            stu_form = Student_Form()
+            return render(request,'admin/adminLogin.html',{'stu_form':stu_form,'msg':message})
+    else:
+        stu_form = Student_Form()
+    return render(request,'admin/adminLogin.html',{'stu_form':stu_form})
